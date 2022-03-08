264647e2e3f2f01876f15f6e594e7b51d6bfc9cf
zc550110 <1363434820@qq.com>
2022-03-08 10:13:51 +0800

    优化提交

--- App.vue
+++ App.vue
@@ -1,71 +1,51 @@
 <script>
-	import {
-		mapState,
-		mapActions
-	} from "vuex";
-	export default {
-		onLaunch: function() {
-			this.cid();
-			this.init();
-		},
-		onShow: function() {},
-		onHide: function() {},
-		computed: {
-			...mapState(["hasLogin", "userInfo"]),
-		},
-		methods: {
-			...mapActions(["getWorkShop"]),
-			init() {
-				// 锁定竖屏
-				// #ifdef APP-PLUS  
-				plus.screen.lockOrientation("portrait-primary");
-				// #endif
-				if (this.hasLogin) {
-					this.getWorkShop();
-					// #ifdef APP-PLUS 
-					uni.reLaunch({
-						url: "pages/index/index"
-					});
-					// #endif
-				}
-			},
-			cid() {
-				// #ifdef APP-PLUS
-				var pinf = plus.push.getClientInfo();
-				console.log(pinf)
-				var cid = pinf.clientid; //客户端标识
-				console.log('cid：' + cid);
+import { mapState, mapActions } from "vuex";
+export default {
+  onLaunch: function () {
+    // this.cid();
+    this.init();
+  },
+  onShow: function () {},
+  onHide: function () {},
+  computed: {
+    ...mapState(["hasLogin", "userInfo"]),
+  },
+  methods: {
+    ...mapActions(["getWorkShop"]),
+    init() {
+      // 锁定竖屏
+      // #ifdef APP-PLUS
+      plus.screen.lockOrientation("portrait-primary"); 
+			// #endif
+      if (this.hasLogin) {
+        uni.reLaunch({ url: "/pages/index/index" });
+      } else {
+        uni.reLaunch({ url: "pages/login/login" });
+      }
+    },
 
-				plus.push.addEventListener('click', (msg)=>{console.log('click:', msg);}, false);
-				
-				plus.push.addEventListener('receive', (msg)=> {
-					plus.runtime.setBadgeNumber(8);
-					plus.push.createMessage('receive', msg);
-				}, false);
-				// #endif
-			}
-			// //#ifdef H5
-			// skipH5() {
-			// 	if (!this.hasLogin) {
-			// 		uni.reLaunch({
-			// 			url: '/'
-			// 		})
-			// 	} else {
-			// 		if (window.location.hash === '#/') {
-			// 			uni.reLaunch({
-			// 				url: '/pages/index/index'
-			// 			})
-			// 		}
-			// 	}
-			// }
-			// //#endif
-		}
-	};
+    cid() {
+      // #ifdef APP-PLUS
+      var pinf = plus.push.getClientInfo();
+      console.log(pinf);
+      var cid = pinf.clientid; //客户端标识
+      console.log("cid：" + cid);
+
+      plus.push.addEventListener("click", (msg) => { console.log("click:", msg)},false);
+
+      plus.push.addEventListener("receive", (msg) => {
+          plus.runtime.setBadgeNumber(8);
+          plus.push.createMessage("receive", msg);
+        },false);
+      // #endif
+    }
+  },
+};
 </script>
 
 <style lang="scss">
-	@import "static/style/font/iconfont.css";
-	@import "static/style/style.scss";
-	@import "uview-ui/index.scss";
-	@import url("components/ly-tree/ly-tree.css");
+@import "static/style/font/iconfont.css";
+@import "static/style/style.scss";
+@import "uview-ui/index.scss";
+@import url("components/ly-tree/ly-tree.css");
 </style>
