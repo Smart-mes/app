197d92dfb562923346ead4a294cc3474663b015a
zc550110 <1363434820@qq.com>
2022-03-09 15:32:05 +0800

    http优化提交

---
+++ api/http.js
@@ -0,0 +1,46 @@
+import config from './config';
+import store from 'store/index'
+/*
+@param url 字符串
+@param method 请求方法
+@param data请求参数
+*/
+const userToken=uni.getStorageSync('userToken');
+const request = (opt) => {
+	const {url,method,data}=opt
+	const headers = {
+		'Content-Type': 'application/json',
+		'Authorization': userToken
+	}
+	 let promise = new Promise(function(resolve, reject) {
+	 uni.request({
+		url: config.api.baseURL + url,
+		method: method,
+		data:data,
+		dataType: 'json',
+		header: headers
+	}).then(result => {
+		let [error, res] = result;
+		if (res.statusCode === 200||res.statusCode === 204||res.statusCode === 201) {
+			resolve(res.data);
+		}else {
+			throw res
+		}	
+	}).catch(error => {
+		let message =error.data.message.toString();
+		switch (error.statusCode) {
+			case 400:
+				uni.showToast({title: message,icon: 'none'})
+				break
+			default:
+				uni.showToast({title: '网络错误',icon: 'none'})				
+				break
+		};
+		reject(error);
+
+	});
+
+ });
+ return promise;
+}
+export default {request};
