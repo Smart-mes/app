2ed65d9459c1e9ac76536662d0ddba64b2a421ef
zc550110 <1363434820@qq.com>
2023-02-13 11:36:34 +0800

    提交HTTP

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 export default {
   api: {
-    baseURL: process.env.NODE_ENV === "development" ? "http://8.129.76.46:8022" : "http://8.129.76.46:8022"
+    baseURL: process.env.NODE_ENV === "development" ? "http://43.139.50.11:8022" : "http://43.139.50.11:8022"
   },
   oeeServerApi: {
     baseURL:"http://8.129.76.46:8090"
--- util/config.js
+++ util/config.js
@@ -1,11 +1,12 @@
 export default {
   api: {
-    baseURL: process.env.NODE_ENV === "development" ? "http://43.139.50.11:8180" : "http://43.139.50.11:8180"
+    baseURL: process.env.NODE_ENV === "development" ? "http://43.139.50.11:8022" : "http://43.139.50.11:8022"
   },
   oeeServerApi: {
     baseURL:"http://8.129.76.46:8090"
   }
 }
+
 
 
 
