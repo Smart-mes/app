14f2026aa08858e97bf920b847b4b6fd938c6e75
zc550110 <1363434820@qq.com>
2023-02-23 11:31:33 +0800

    提交配置

--- util/config.js
+++ util/config.js
@@ -1,9 +1,9 @@
 export default {
   api: {
-    baseURL: process.env.NODE_ENV === "development" ? "http://8.129.76.46:8022" : "http://8.129.76.46:8022"
+    baseURL: process.env.NODE_ENV === "development" ? "http://43.139.50.11:8022" : "http://43.139.50.11:8022"
   },
   oeeServerApi: {
-    baseURL:"http://8.129.76.46:8090"
+    baseURL:"http://43.139.50.11:8090"
   }
 }
 
