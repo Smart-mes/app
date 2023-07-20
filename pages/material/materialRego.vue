97965cc86d592845a1d9609ea62b89e3acb1c05d
zc550110 <1363434820@qq.com>
2023-07-20 14:57:27 +0800

    物料注册提交

--- pages/material/materialRego.vue
+++ pages/material/materialRego.vue
@@ -26,6 +26,7 @@
 
 			 </ex-form>
 		</ex-box>
+		<u-toast ref="uToast" />
 		<view class="h-100"></view>
     <view class="fixBtn">
 			<u-row gutter="20">
@@ -50,23 +51,34 @@
 					formData:{machineCode:'',matCode:'',lotNo:'',inputQty:1},
 					formItem:[
 							{ label: "当前设备", props: "machineCode", type: "exInput",border: true,disabled:true},
-							{ label: "物料编号", props: "matCode", type: "exInput",border: true},
-							{ label: "物料批次", props: "lotNo", type: "exInput",border: true},
+							{ label: "物料编号", props: "matCode", type: "exInput",border: true,change:true,focus:true},
+							{ label: "物料批次", props: "lotNo", type: "exInput",border: true,change:true,focus:false},
 							{ label: "批次数量", props: "inputQty", type: "numberBox",disabled:true},
 					],
 				rules: {},
 				event:{
 					matCode:{
 						confirm:async (e)=>{
-									const res=await this.matCodeFetch(e);
-									if(res.length) this.matCodeData=res[0];												
-								},
+								const res=await this.matCodeFetch(e);
+								if(res.length) {
+									this.matCodeData=res[0];
+							    this.getFocus(true);
+								}								
+						},
+						input:(e)=>{							
+						  if(!e) this.matCodeClear();
+					 }								
 					},
 					lotNo:{
 						confirm:async (e)=>{
 								const res=await this.lotNoFetch(e);
 								this.setType(res.length);
 						},
+						input:(e)=>{							
+							if(!e){
+								this.lotNoClear();
+							}
+					 }
 					}	
 				},			
 				},
@@ -75,7 +87,7 @@
 				matCodeData:{},
 				isLotNo:false,
 				isOut:true,
-				isRego:true
+				isRego:true,
 			}
 		},
 		computed: {
@@ -83,6 +95,15 @@
     },
 		methods: {
 			...mapMutations(['set_line','clear_storage']),
+			lotNoClear(){
+				this.isLotNo=false;
+			},
+			matCodeClear(){
+				this.matCodeData={};
+			},
+			toast(msg){
+				this.$refs.uToast.show({title:msg,type:'success',position:'bottom'})
+			},		
 			machineChange(){
 				this.clear_storage();
 				uni.reLaunch({ url:'/pages/index/index' });
@@ -90,27 +111,46 @@
 			async checkOutHandle(){
 				const {lotNo}=this.$refs.regoForm.formData;
 				await this.checkOutFetch(lotNo);
+				await this.toast('安装成功');
+				await this.$refs.regoForm.clear();
 				await this.clearData();
+				await this.getFocus(false);
+			},
+			getFocus(isLotNo){
+				this.formOpts.formItem[1].focus=!isLotNo;		
+				this.formOpts.formItem[2].focus=isLotNo;	
 			},
 			async checkInHandle(){
-			  const regoForm=this.$refs.regoForm
 				await this.checkInFetch({
 					stationCode:this.stationCode,
-					...regoForm.formData,
+					...this.$refs.regoForm.formData,
 					workshop:this.line[0].value,
-					lotQty:regoForm.inputQty
+					lotQty:this.$refs.regoForm.inputQty
 				});
+				await this.toast('卸载成功');
+				await this.$refs.regoForm.clear();
 				await this.clearData();
+				await this.getFocus(false);
 			},
 			clearData(){
 				this.$refs.regoForm.clear();
-        this.setType(false);
+        this.setType(true);
+				this.lotNoClear();
+			  this.matCodeClear();
+				this.formOpts.formItem[1].focus=true;		
+				this.formOpts.formItem[2].focus=false;		
 			},
 			setType(isType){
+			const {matCode,lotNo}=this.$refs.regoForm.formData
 				this.isLotNo=!!isType;
 				this.formOpts.formItem[3].disabled=!!isType;
-				this.isOut=	!isType;
-				this.isRego=!!isType;
+				if(!matCode&&!lotNo){
+					this.isOut=	true;
+				  this.isRego=true;
+				}else{
+					this.isOut=	!isType;
+				  this.isRego=!!isType;
+				}
 			},
 			BStationFetch(stationCode){
 				return this.$http.request({url: '/api/BStationList',method: "GET",data: {stationCode}}); 
