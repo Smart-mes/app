e47990e1729d59eaebfe24e35164c84a0ce6383a
zc550110 <1363434820@qq.com>
2023-07-27 15:43:33 +0800

    提交HTTP

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 export default {
   api: {
-    baseURL: process.env.NODE_ENV === "development" ? "http://43.139.50.11:8037" : "http://43.139.50.11:8037"
+    baseURL: process.env.NODE_ENV === "development" ? "http:// 192.168.1.71:8002" : "http://192.168.2.235:8012"
   },
   oeeServerApi: {
     baseURL:"http://43.139.50.11:8090"
@@ -8,6 +8,7 @@ export default {
 }
 
 // http://43.139.50.11:8037
-// http://192.168.2.235:8012
+//华旺: http://192.168.2.235:8012
+// 郭：http:// 192.168.1.71:8002
 
 
